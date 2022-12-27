d852433b24b0eb3e61ec3db6e6e30655a308f000
Haritha Rathnayake <chamararathnayaka@outlook.com>
2022-12-28 02:04:50 +0530

    Created socket on server

--- src/Crimewatch-Backend/src/app.ts
+++ src/Crimewatch-Backend/src/app.ts
@@ -2,13 +2,29 @@ import express from "express";
 import Default from "./Configs/Default";
 import DbContext from "./Context/DbContext";
 import Routes from "./Routes/Routes";
+import { createServer } from "http";
+import { Server } from "socket.io";
 
 const app = express();
 
 app.use(express.json({ limit: "50mb" }));
 app.use("/API", Routes);
 
-app.listen(Default.server.port!, async () => {
+const httpServer = createServer(app);
+const io = new Server(httpServer);
+
+io.on("connection", (socket) => {
+    console.log("User connected");
+    socket.on("disconnect", () => {
+        console.log("User disconnected");
+    });
+    socket.on("message", (socket) => {
+        console.log(socket);
+        io.emit("message", { type: "new-message", text: socket });
+    });
+});
+
+httpServer.listen(Default.server.port!, async () => {
     await DbContext.ConnectDb();
     console.log(
         `Application listening at http://localhost:${Default.server.port!}`
