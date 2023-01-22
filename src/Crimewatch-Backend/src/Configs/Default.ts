3bd670a6393895068d9021f62b1becc7b5bb1ee7
Haritha Rathnayake <chamararathnayaka@outlook.com>
2023-01-22 10:47:52 +0530

    Dissabled sockt.io

--- src/Crimewatch-Backend/src/Configs/Default.ts
+++ src/Crimewatch-Backend/src/Configs/Default.ts
@@ -6,6 +6,7 @@ const MONGO_URL = process.env.MONGO_URL;
 const MONGO_URL_TEST = process.env.MONGO_URL_TEST;
 const MONGO_URL_PROD = process.env.MONGO_URL_PROD;
 const SERVER_PORT = process.env.SERVER_PORT;
+const SERVER_HOST = process.env.SERVER_HOST;
 
 export default {
     mongo: {
@@ -14,6 +15,7 @@ export default {
         productionurl: MONGO_URL_PROD,
     },
     server: {
+        name: SERVER_HOST,
         port: SERVER_PORT,
     },
 };
